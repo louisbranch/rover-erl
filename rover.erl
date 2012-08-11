d4d7fee13a69c90d05b892a3aaf6618840213453
Luiz Branco <artfikan@gmail.com>
2012-08-11 00:07:08 -0300

    Add initial logic to turn

--- rover.erl
+++ rover.erl
@@ -1,17 +1,44 @@
 -module(rover).
--export([init/1]).
+-export([init/1,turn/2]).
 
-init(Loc) ->
+init(Position) ->
   receive
-    Instructions ->
-      Cmds = parse(Instructions),
+    {move, Instructions} ->
+      Cmds = parse(Position, Instructions),
       io:format("~p~n", [Cmds]),
-      init(Loc)
+      init(Position);
+    _Else ->
+      io:format("unknown command")
   end.
 
-parse([]) -> [];
-parse([H|T]) -> [cmd(H)|parse(T)].
+parse(_, []) -> [];
+parse(Position, [H|T]) -> [cmd(Position, H)|parse(Position, T)].
 
-cmd(77) -> foward;
-cmd(82) -> right;
-cmd(76) -> left.
+cmd(Position, Command) ->
+  case Command of
+    $R -> turn(right, Position);
+    $L -> turn(left, Position);
+    $M -> move(foward, Position)
+  end.
+
+turn(Direction, Position) ->
+  [X,_,Y,_,Cardinal] = Position,
+  NewCardinal = case Direction of
+    right ->
+      case Cardinal of
+        $N -> $E;
+        $E -> $S;
+        $S -> $W;
+        $W -> $N
+      end;
+    left ->
+      case Cardinal of
+        $N -> $W;
+        $W -> $S;
+        $S -> $E;
+        $E -> $N
+      end
+  end,
+  [X,$ ,Y,$ ,NewCardinal].
+
+move(foward, _) -> "Alright".
